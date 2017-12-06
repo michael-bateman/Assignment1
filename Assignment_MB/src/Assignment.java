beaab8f9a5eb8e8ce650cf421825ec89ac8f9cb9
Michael Bateman <info@michaelbateman.ca>
2017-12-06 18:13:33 -0500

    Update

--- Assignment_MB/src/Assignment.java
+++ Assignment_MB/src/Assignment.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * @author michael.bateman
  *
  */
-public class Assignment_MB2 {
+public class Assignment {
 	
 	/**
 	 * 
@@ -85,9 +85,9 @@ public class Assignment_MB2 {
 		
 		Question q = new Question(inputdata[0]);
 		
-		String[] question = q.getQuestion();
-		String[] answer = q.getAnswer();
-		String[] answerselect = {"A","B","C","D"};
+		String[] question = q.getQuestion(); // gets the questions from the Question class
+		String[] answer = q.getAnswer(); // gets the answers from the Answer class
+		String[] answerselect = {"A","B","C","D"}; // used for randomizing the multiple choice answers and selections
 		
 		Scanner s = new Scanner(System.in);
 		
@@ -98,6 +98,9 @@ public class Assignment_MB2 {
 		int arandom = 0; // for choosing shuffle order of responses
 		double scorepercent = 0.0; // sets your score to 0 - note, it's a double
 		int questioncount = 0; // this is the question number to be used as a question header
+		int reviewcount = 0;
+		int random = 0;
+		boolean add = true;
 			
 		do {
 			if (intinput <= 0 || intinput > question.length) { // if statement to check to see if the input is invalid
@@ -107,6 +110,16 @@ public class Assignment_MB2 {
 			intinput = s.nextInt(); // takes an int input
 		} while (intinput <= 0 || intinput > question.length); // if the statement is invalid
 		
+		/*
+		 * reviewquestion[] - used for adding the questions that were incorrectly answered
+		 * reviewanswer[] - used for adding the answers that were incorrectly answered
+		 * reviewyourchoice[] - used for adding the answers that were incorrectly answered
+		 */
+		
+		String[] reviewquestion = new String[intinput];
+		String[] reviewanswer = new String[intinput];
+		String[] reviewyourchoice = new String[intinput];
+		
 		for (int i = 0; i < intinput; i++) { // this is the question loop
 			
 			do {
@@ -114,9 +127,8 @@ public class Assignment_MB2 {
 			} while (question[grandom] == null); // this will check to see if the question has already been used
 			
 			String[] answersolutions = new String[4]; //4 is the amount of MC responses
-			answersolutions[0] = answer[grandom];
-			int random = 0;
-			boolean add = true;
+			answersolutions[0] = answer[grandom]; // selects the answer from the question randomizer above
+			
 			for (int a = 1; a < answersolutions.length; a++) {
 				add = true;
 				random = (int)(Math.random() * answer.length);
@@ -138,7 +150,7 @@ public class Assignment_MB2 {
 			questioncount++;
 			System.out.println("Question #" + questioncount + ":");
 			System.out.println(inputdata[1] + " " + question[grandom] + "?");
-			question[grandom] = null;
+			//question[grandom] = null;
 			String[] answersolutionsfinal = new String[4];
 			String letteranswer = "";
 			for (int c = 0; c < answersolutions.length; c++) {
@@ -156,23 +168,58 @@ public class Assignment_MB2 {
 					System.out.println("Invalid Answer... Please try again");
 				}
 				System.out.println("A) " + answersolutionsfinal[0] + inputdata[2] + "\nB) " + answersolutionsfinal[1] + inputdata[2] + "\nC) " + answersolutionsfinal[2] + inputdata[2] + "\nD) " + answersolutionsfinal[3] + inputdata[2]);
+				System.out.print("Answer: ");
 				strinput = s.next().toUpperCase();
 			} while (strinput.length() >= 2 || !strinput.equals("A") && !strinput.equals("B") && !strinput.equals("C") && !strinput.equals("D"));
-			if (strinput.equals(letteranswer)) {
+			if (strinput.equals(letteranswer)) { // correct
 				score++;
 				System.out.print("Correct!");
-			} else {
+			} else { // incorrect 
 				System.out.print("Incorrect! The correct answer was " + letteranswer + "!");
+				reviewquestion[i] = question[grandom];
+				reviewanswer[i] = answer[grandom];
+				for (int d = 0; d < answersolutions.length; d++) {
+					if (strinput.equals(answerselect[d])) {
+						reviewyourchoice[i] = answersolutionsfinal[d] + inputdata[2];
+						d = answersolutions.length; // this will end the loop without forcing break
+					}
+				} 
 			}
 			
 			if (i != intinput-1) {
 				System.out.println(" Your score is " + score);
 			}
+			question[grandom] = null; // we need to set null down here because we reaccess the array for review if wrong.
 			
-		}
+		} //end game loop
 		scorepercent = Math.round(((double)(score)/intinput) * 10000.0)/100.0;
 		System.out.println("\nYou got " + scorepercent + "% of the questions correct");
-		
+		// add an if statement to see if they got 100%
+		if (score != intinput) {
+			strinput = "Y";
+			do {
+				if (!strinput.equals("Y") && !strinput.equals("N")) {
+					System.out.println("Invalid Answer... Please try again");
+				}
+				System.out.print("Would you like to review your incorrect questions? (Y/N) ");
+				strinput = s.next().toUpperCase();
+			} while (!strinput.equals("Y") && !strinput.equals("N"));
+			
+			if (strinput.equals("Y")) {
+				for (int e = 0; e < reviewquestion.length; e++) {
+					reviewcount++;
+					if (reviewquestion[e] != null) {
+						System.out.println("Review #" + reviewcount + ": " + inputdata[1] + " " +  reviewquestion[e] + "?");
+						System.out.println("Answer: " + reviewanswer[e]);
+						System.out.println("You chose: " + reviewyourchoice[e]);
+						System.out.print("Press any key to continue: ");
+						strinput = s.next(); // just acts as a wait
+					} else {
+						reviewcount = reviewcount - 1;
+					}
+				}
+			}
+		}// end answer review
 	}
 	public static void main(String[] args) {
 		String inputagain = "Y";
@@ -183,11 +230,11 @@ public class Assignment_MB2 {
 				if (inputagain.length() >= 2 || !inputagain.equals("Y") && !inputagain.equals("N")) {
 					System.out.println("Invalid Answer... Please try again");
 				}
-				System.out.println("Would you like to play again? (Y/N)");
+				System.out.println("\nWould you like to play again? (Y/N)");
 				inputagain = sa.next().toUpperCase();
 			} while(inputagain.length() >= 2 || !inputagain.equals("Y") && !inputagain.equals("N"));
 		} while (inputagain.equals("Y"));
 		System.out.println("Thanks for playing! Have a great day!");
-	}
+	} // end main
 
-}
+} // end class
